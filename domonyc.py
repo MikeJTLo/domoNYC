70ec32b5fd86a8f9fea4a937f4d8a8045ec04041
Michael Lo <lo.michael@gmail.com>
2013-07-16 21:10:05 -0400

    Update domonyc.py

--- domonyc.py
+++ domonyc.py
@@ -1,7 +1,14 @@
+"""
+	Project domoNYC
+	---------------
+	
+	:copyright: (c) 2013 by Michael Lo
+	:All Rights Reserved.
+"""
+
 import os
 from flask import Flask
 #from jinja2 import Environment, PackageLoader
-
 #env = Environment(loader=PackageLoader('domoNYC','templates'))
 
 app = Flask(__name__)
@@ -9,6 +16,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Let\'s see how long you have lived in days, minutes and seconds.<br /><iframe width=\'500\' height=\'300\' frameBorder=\'0\' src=\'http://a.tiles.mapbox.com/v3/michaeljlo.map-wlxizy2a.html#4/40.03918647233613/-96.61869964833944\'></iframe>'
+
+@app.route('/map')
+def hello():
+    return '<iframe width=\'500\' height=\'300\' frameBorder=\'0\' src=\'http://a.tiles.mapbox.com/v3/michaeljlo.map-wlxizy2a.html#4/40.03918647233613/-96.61869964833944\'></iframe>'
+
+
 
 if __name__ == '__main__':
 	app.run(debug=False)
